777d8d768136c3aed6991e768577a3985f834b8e
hink04 <hahaha11kr@naver.com>
2026-08-18 17:03:48 +0900

    create listen, clientsocket1,2(server.cpp)

--- server/server.cpp
+++ server/server.cpp
@@ -42,6 +42,45 @@ int main()
         cout<<"bind success\n";
     }
 
+    if(listen(serverSocket,SOMAXCONN)==SOCKET_ERROR){//클라이언트 연결대기
+        cout<<"listen failed\n";
+        return 1;
+    }
+    else{
+        cout<<"listen success\n";
+    }
+
+    sockaddr_in clientadd1{};//클라이언트 연결을 위한 클라이언트소켓구조체1 생성
+    int clientsize1=sizeof(clientadd1);
+
+    SOCKET clientsocket1=accept(serverSocket,(sockaddr*)&clientadd1,&clientsize1);
+
+    if(clientsocket1==INVALID_SOCKET){
+        cout<<"client1 accept failed\n";
+        return 1;
+    }
+    else{
+        cout<<"client1 accept connected\n";
+    }
+
+     sockaddr_in clientadd2{};//클라이언트 연결을 위한 클라이언트소켓구조체1 생성
+    int clientsize2=sizeof(clientadd2);
+
+    SOCKET clientsocket2=accept(serverSocket,(sockaddr*)&clientadd2,&clientsize2);
+
+    if(clientsocket2==INVALID_SOCKET){
+        cout<<"client2 accept failed\n";
+        closesocket(clientsocket1);
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+    else{
+        cout<<"client2 accept connected\n";
+    }
+
+
+
 
 
     return 0;
